3686e508d7eaf19d08fe43c2a0d6f4ce8df97c76
Alejandro <a.alvarezmontes@gmail.com>
2018-06-28 16:53:03 -0400

    Cambios ContactForm

--- submit.php
+++ submit.php
@@ -18,7 +18,7 @@ $message = $_POST['message'];
 // Create the email and send the message
 $to = 'a.alvarezmontes@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contacto de la web:  $name";
-$email_body = "Tienes un mensaje desde el formulario de contacto de tu portafolio.\n\n"."Los detalles son:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_body = "Tienes un mensaje desde el formulario de contacto de tu portafolio.\n\n"."Los detalles son:\n\nNombre: $name\n\nEmail: $email_address\n\nTelefono: $phone\n\nComentario:\n$message";
 $headers = "From: noreply@alejandroalvarez.us\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
