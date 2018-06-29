3a3c7cbb6a2e847aa249426eec74db4203fec77d
Alejandro Alvarez <a.alvarezmontes@gmail.com>
2018-06-28 20:42:00 -0400

    Arreglo Formulario mensajes y limpiar ok

--- js/contact.js
+++ js/contact.js
@@ -30,8 +30,8 @@ $(function() {
                 success: function() {
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
+                    /*$('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");*/
                     $('#success > .alert-success')
                         .append("<strong>Tu mensaje a sido enviado. </strong>");
                     $('#success > .alert-success')
